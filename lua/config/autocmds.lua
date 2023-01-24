d2b36e5b6ef2699b8f92ff44ee497c1ce17e1ee7
Marcus Kellerman <sharkus@gmail.com>
2023-01-24 09:34:42 -0800

    Added support for transparent background, removed lazy-lock.json from git

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set a transparent background
+vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = function()
+        vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+        vim.api.nvim_set_hl(0, "Float", {bg = "none"})
+        require("notify").setup({
+            background_colour = "#000000",
+        })
+    end
+})
--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -2,6 +2,6 @@ return {
   { "shaunsingh/nord.nvim" },
   { "ellisonleao/gruvbox.nvim" },
   { "LazyVim/LazyVim", opts = {
-    colorscheme = "gruvbox",
+    colorscheme = "nord",
   } },
 }
