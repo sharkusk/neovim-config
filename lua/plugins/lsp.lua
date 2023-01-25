cc293287e24a09e39767634801db1e3af176a33b
Marcus Kellerman <sharkus@gmail.com>
2023-01-24 17:19:23 -0800

    Do not allow Mason to install jsonls on MacOS as it doesn't work

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,6 +9,7 @@
 -- * override the configuration of LazyVim plugins
 --require("lspconfig").clangd.setup({})
 local machine = vim.loop.os_uname().machine
+local sys = vim.loop.os_uname().sysname
 if machine == "aarch64" then
   return {
     "neovim/nvim-lspconfig",
@@ -17,6 +18,18 @@ if machine == "aarch64" then
       ---@type lspconfig.options
       servers = {
         clangd = { mason = false },
+      },
+      autoformat = false,
+    },
+  }
+elseif sys == "Darwin" then
+  return {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        jsonls = { mason = false },
       },
       autoformat = false,
     },
